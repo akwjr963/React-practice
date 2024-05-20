cd668741f2ea639872aaffb8d7eaf3e0a417ab8a
akwjr963 <akwjr963@daum.net>
2024-05-20 15:24:08 +0900

    폴더 재설정

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Gallery from './components/gallery';
+
+function App() {
+    return (
+        <div>
+            <h1>Image Gallery</h1>
+            <p>이미지를 검색하십시오</p>
+            <Gallery />
+        </div>
+    );
+}
+
+export default App;
